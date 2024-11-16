694a389bf4e1f7306e661591dac2729f732fe422
densumesh <dens.sumesh79@gmail.com>
2024-11-16 16:49:25 -0700

    cleanup: make it look nicer

--- src/pages/CourseDashboard.tsx
+++ src/pages/CourseDashboard.tsx
@@ -1,17 +1,25 @@
 import { Navbar } from "../components/Navbar";
 import { NeedHelp } from "../components/NeedHelp";
 import CourseCard from "../components/CourseCard";
-import BikeIcon from '../assets/BikeIcon.svg';
-import PlaneIcon from '../assets/PlaneIcon.svg';
-
-import { useState } from "react";
+import BikeIcon from "../assets/BikeIcon.svg";
+import PlaneIcon from "../assets/PlaneIcon.svg";
 
 export const CourseDashboard = () => {
-  const [courses, setCourses] = useState([
+  const courses = [
     { icon: BikeIcon, name: "Bike 1", totalModules: 10, completedModules: 5 },
-    { icon: PlaneIcon, name: "Plane Cockpit 1", totalModules: 6, completedModules: 2 },
-    { icon: PlaneIcon, name: "Plane Cockpit 2", totalModules: 13, completedModules: 0 }
-  ]);
+    {
+      icon: PlaneIcon,
+      name: "Plane Cockpit 1",
+      totalModules: 6,
+      completedModules: 2,
+    },
+    {
+      icon: PlaneIcon,
+      name: "Plane Cockpit 2",
+      totalModules: 13,
+      completedModules: 0,
+    },
+  ];
 
   return (
     <div className="w-full h-screen bg-gradient-to-t from-transparent via-purple-100 to-transparent">
@@ -19,20 +27,20 @@ export const CourseDashboard = () => {
         <Navbar />
         <NeedHelp />
 
-      <div className="flex justify-center h-3/5 p-8">
-        <div className="course-containers w-4/5 flex flex-col gap-4">
-          {courses.map((course, index) => (
-        <CourseCard
-          key={index}
-          icon={course.icon}
-          name={course.name}
-          totalModules={course.totalModules}
-          completedModules={course.completedModules}
-        />
-          ))}
+        <div className="flex justify-center h-3/5 py-8">
+          <div className="course-containers w-4/5 flex flex-col gap-4">
+            {courses.map((course, index) => (
+              <CourseCard
+                key={index}
+                icon={course.icon}
+                name={course.name}
+                totalModules={course.totalModules}
+                completedModules={course.completedModules}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
