3117fb57b2c54745ff1149958295d9d14f2c531e
densumesh <dens.sumesh79@gmail.com>
2024-11-16 16:28:16 -0700

    feature: finish chatbot and expert screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ export function Navbar() {
     <header className="bg-white border border-b-2">
       <nav
         aria-label="Global"
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 py-2 lg:px-8">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Virtuosim</span>
